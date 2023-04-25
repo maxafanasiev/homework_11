6f753173fec26c68bc124b6a5036d062cbe22f73
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-04-25 22:42:32 +0300

    add serialize address_book

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import os
 import oop
 
 
-adress_book = oop.AddressBook()
+address_book = oop.AddressBook()
 bot_working = True
 clear = lambda: os.system('clear')
 
-#for test
-a1 = oop.Name()
-a1.value = 'Max'
-b1 = oop.Phone()
-b1.value = '12'
-c1 = oop.Birthday()
+# #for test
+# a1 = oop.Name()
+# a1.value = 'Max'
+# b1 = oop.Phone()
+# b1.value = '12'
+# c1 = oop.Birthday()
 
-a2 = oop.Name()
-a2.value = 'Ann'
-b2 = oop.Phone()
-b2.value = '13'
-c2 = oop.Birthday()
-c2.value = '12-12-2001'
-rec1 = oop.Record(a1,b1,c1)
-rec2 = oop.Record(a2,b2,c2)
+# a2 = oop.Name()
+# a2.value = 'Ann'
+# b2 = oop.Phone()
+# b2.value = '13'
+# c2 = oop.Birthday()
+# c2.value = '12-12-2001'
+# rec1 = oop.Record(a1,b1,c1)
+# rec2 = oop.Record(a2,b2,c2)
 
-add_ph = oop.Phone()
-add_ph.value = '34343434'
-rec1.add_phone(add_ph)
-adress_book.addRecord(rec1)
-adress_book.addRecord(rec2)
+# add_ph = oop.Phone()
+# add_ph.value = '34343434'
+# rec1.add_phone(add_ph)
+# address_book.addRecord(rec1)
+# address_book.addRecord(rec2)
 
 
 def input_error(func):
@@ -52,11 +52,14 @@ def input_error(func):
 
 def start():
     clear()
-    print(f"CLI Address book bot is running...")
+    address_book.load_from_file("address_book.json")
+    print("CLI Address book bot is running...")
+    print("Address book loaded.")
 
 
 def show_page(page_number=1, count=5):
-    return adress_book.show_page(page_number, count)
+    clear()
+    return address_book.show_page(page_number, count)
 
 
 def helper():
@@ -71,7 +74,8 @@ def close():
     clear()
     global bot_working
     bot_working = False
-    return ("Good bye!")
+    address_book.save_to_file('address_book.json')
+    return ("Good bye!\n Bot stopped.")
 
 
 def hello():
@@ -93,13 +97,13 @@ def add_record(name, phone='' ,birthday=''):
 
     rec = oop.Record(rec_name, rec_phone, rec_bd)
 
-    adress_book.addRecord(rec)
+    address_book.addRecord(rec)
     return rec.print_record()
 
 
 def change_phone(name, old_phone, new_phone):
     clear()
-    rec = adress_book[name]
+    rec = address_book[name]
     o_ph = oop.Phone()
     o_ph.value = old_phone
     n_ph = oop.Phone()
@@ -110,7 +114,7 @@ def change_phone(name, old_phone, new_phone):
 
 def add_phone(name, phone):
     clear()
-    rec = adress_book[name]
+    rec = address_book[name]
     ph = oop.Phone()
     ph.value = phone
     rec.add_phone(ph)
@@ -119,7 +123,7 @@ def add_phone(name, phone):
 
 def add_birthday(name, birthday):
     clear()
-    rec = adress_book[name]
+    rec = address_book[name]
     bd = oop.Birthday()
     bd.value = birthday
     rec.add_birthday(bd)
@@ -128,7 +132,7 @@ def add_birthday(name, birthday):
 
 def delete_phone(name, phone):
     clear()
-    rec = adress_book[name]
+    rec = address_book[name]
     ph = oop.Phone()
     ph.value = phone
     rec.del_phone(ph)
@@ -137,19 +141,19 @@ def delete_phone(name, phone):
 
 def days_to_birthday(name):
     clear()
-    rec = adress_book[name]
+    rec = address_book[name]
     return rec.days_to_birthday()
 
 
 def showall():
     clear()
-    return adress_book.print_all()
+    return address_book.print_all()
 
 
 
 def phone(name):
     clear()
-    rec = adress_book[name]
+    rec = address_book[name]
     return rec.print_record()
 
 
